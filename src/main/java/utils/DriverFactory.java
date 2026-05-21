4623dfd1857d99805385624ec763ef0878ec7cff
trangntt219 <nguyentrang721991@gmail.com>
2026-05-21 17:06:24 +0700

    automation_v0.9

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -1,13 +1,26 @@
 package utils;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class DriverFactory {
 
-    public static WebDriver initDriver() {   // ✅ phải trả về WebDriver
+    public static WebDriver initDriver() {
+
         WebDriverManager.chromedriver().setup();
-        return new ChromeDriver();          // ✅ return driver
+
+        ChromeOptions options = new ChromeOptions();
+        if (System.getenv("CI") != null) {
+            options.addArguments("--headless=new");
+        }
+        //  QUAN TRỌNG CHO GITHUB ACTIONS
+        options.addArguments("--headless=new");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--window-size=1920,1080");
+
+        return new ChromeDriver(options);
     }
 }
